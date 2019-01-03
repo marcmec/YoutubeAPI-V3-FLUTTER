750b1e965f5a24e8b23ec1682d7c333b893bcdac
marcmec <viniciuslag@gmail.com>
2019-01-03 02:52:40 -0300

    add image in data payload

--- youtubeapiv3/lib/src/videoplaylist.dart
+++ youtubeapiv3/lib/src/videoplaylist.dart
@@ -4,57 +4,75 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class VideoPlayList extends StatefulWidget{
-  VideoPlayList({this.title,this.url});
+class VideoPlayList extends StatefulWidget {
+  VideoPlayList({this.title, this.url});
   final String title;
   final String url;
   //apikey AIzaSyC8R3c_heItD8yntPYvWQS88zBEn8XUGyY
   //playlist id PLFlfI-YjrJuRazkbnVC1tkRDVKeOBjgAu
   @override
-  _StatePLaylist createState()=> new _StatePLaylist();
+  _StatePLaylist createState() => new _StatePLaylist();
 }
 
-class _StatePLaylist extends State<VideoPlayList>{
-Map data;
-Future<List> getData() async{
-  final response = await http.get(widget.url);
+class _StatePLaylist extends State<VideoPlayList> {
+  Map data;
+  Future<List> getData() async {
+    final response = await http.get(widget.url);
 
-  data =json.decode(response.body);
+    data = json.decode(response.body);
 
-  return data["items"];
-  
+    return data["items"];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new FutureBuilder<List>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? new ListVideo(list: snapshot.data)
+              : new CircularProgressIndicator();
+        },
+      ),
+    );
+  }
 }
-@override
-Widget build(BuildContext context){
-  
-  return new Scaffold(
 
-appBar: new AppBar(title: new Text(widget.title),),
-body: new FutureBuilder<List>(
-  future: getData(),
-  builder: (context, snapshot){
-    if(snapshot.hasError) print(snapshot.error);
-    return snapshot.hasData ? new ListVideo(list: snapshot.data) : new CircularProgressIndicator();
-      },
-    ),
-      );
-        }
-        }
-    
-    class ListVideo extends StatelessWidget{
-      final List list;
-      ListVideo ({this.list});
-      @override
-      Widget build(BuildContext context){
+class ListVideo extends StatelessWidget {
+  final List list;
+  ListVideo({this.list});
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      shrinkWrap: true,
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, index) {
+        return new Container(
+          padding: EdgeInsets.all(10),
+          child: new Column(
+            children: <Widget>[
+              new Container(
+                height: 160,
+                width: 260,
+                  decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: new NetworkImage(
+                    list[index]["snippet"]["thumbnails"]["high"]["url"]
+                  ),
+                  fit: BoxFit.cover
+                ),
+              )),
 
-        return new ListView.builder(
-            shrinkWrap: true,
-            itemCount: list==null?0 :list.length,
-            itemBuilder: (context, index){
-              return new Text(list[index]["snippet"]["title"]);
-            },
+              Text(list[index]["snippet"]["title"]),
+            ],
+          ),
         );
-      }
+      },
+    );
+  }
 }
-    
-  
