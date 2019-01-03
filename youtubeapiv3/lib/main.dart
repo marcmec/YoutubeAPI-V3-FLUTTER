c64704bcf3ef302a452af88d31ee78d6ec95be39
marcmec <viniciuslag@gmail.com>
2019-01-03 02:19:42 -0300

    first commit with return text payloads.

---
+++ youtubeapiv3/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:youtubeapiv3/src/videoplaylist.dart';
+
+void main() => runApp(new YouApp());
+
+class YouApp extends StatefulWidget {
+  @override
+  _StateYouApp createState() => new _StateYouApp();
+}
+
+class _StateYouApp extends State<YouApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Youtube Api V3",
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          SizedBox(
+              height: 90,
+              child: Card(
+                color: Colors.blue,
+              )),
+          Row(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(10)),
+              Icon(Icons.video_library),
+              FlatButton(
+                child: Text(
+                  "Playlist",
+                  textScaleFactor: 1.7,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(new MaterialPageRoute(
+                    builder: (BuildContext context)=> new VideoPlayList(
+                      url:"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLFlfI-YjrJuRazkbnVC1tkRDVKeOBjgAu&maxResults=10&key=AIzaSyC8R3c_heItD8yntPYvWQS88zBEn8XUGyY",title: "YOUTUBE",)
+                  ));
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ youtubeapiv3/lib/src/videoplaylist.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class VideoPlayList extends StatefulWidget{
+  VideoPlayList({this.title,this.url});
+  final String title;
+  final String url;
+  //apikey AIzaSyC8R3c_heItD8yntPYvWQS88zBEn8XUGyY
+  //playlist id PLFlfI-YjrJuRazkbnVC1tkRDVKeOBjgAu
+  @override
+  _StatePLaylist createState()=> new _StatePLaylist();
+}
+
+class _StatePLaylist extends State<VideoPlayList>{
+Map data;
+Future<List> getData() async{
+  final response = await http.get(widget.url);
+
+  data =json.decode(response.body);
+
+  return data["items"];
+  
+}
+@override
+Widget build(BuildContext context){
+  
+  return new Scaffold(
+
+appBar: new AppBar(title: new Text(widget.title),),
+body: new FutureBuilder<List>(
+  future: getData(),
+  builder: (context, snapshot){
+    if(snapshot.hasError) print(snapshot.error);
+    return snapshot.hasData ? new ListVideo(list: snapshot.data) : new CircularProgressIndicator();
+      },
+    ),
+      );
+        }
+        }
+    
+    class ListVideo extends StatelessWidget{
+      final List list;
+      ListVideo ({this.list});
+      @override
+      Widget build(BuildContext context){
+
+        return new ListView.builder(
+            shrinkWrap: true,
+            itemCount: list==null?0 :list.length,
+            itemBuilder: (context, index){
+              return new Text(list[index]["snippet"]["title"]);
+            },
+        );
+      }
+}
+    
+  
